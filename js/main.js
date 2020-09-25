471f442a1b27e2df124a7f4ae909d2172baf65d4
Mensur <mensurdurakovic5@gmail.com>
2020-09-25 20:26:29 +0200

    added sidedrawer menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+let sidedrawerOpen = false;
+let activeSidedrawerItem = "home";
+let activeLanguage = "spanish";
+
 function scaleToFill() {
-  $("video.video-background").each(function (index, videoTag) {
-    var $video = $(videoTag),
-      videoRatio = videoTag.videoWidth / videoTag.videoHeight,
-      tagRatio = $video.width() / $video.height(),
-      val;
+  $("video.video-background").each(function (_, videoTag) {
+    const $video = $(videoTag);
+    const videoRatio = videoTag.videoWidth / videoTag.videoHeight;
+    const tagRatio = $video.width() / $video.height();
+    let val;
 
     if (videoRatio < tagRatio) {
       val = (tagRatio / videoRatio) * 1.02; // size increased by 2% because value is not fine enough and sometimes leaves a couple of white pixels at the edges
@@ -15,11 +19,30 @@ function scaleToFill() {
   });
 }
 
+function toggleSidedrawer() {
+  console.log("toggleSidedrawer", sidedrawerOpen);
+  if (!sidedrawerOpen) {
+    $(".sidedrawer").addClass("sidedrawer-active");
+    //$(".sidedrawer").show("slide", { direction: "right" }, 1000);
+    sidedrawerOpen = true;
+  } else {
+    $(".sidedrawer").removeClass("sidedrawer-active");
+    //$(".sidedrawer").hide("slide", { direction: "right" }, 1000);
+    sidedrawerOpen = false;
+  }
+}
+
+function setActiveSidedrawerItem(newActiveSidedrawerItem) {
+  $(".sidedrawer-menu-body-item-text").removeClass(
+    "sidedrawer-menu-body-item-active"
+  );
+  console.log("setActiveSidedrawerItem", newActiveSidedrawerItem);
+  $(newActiveSidedrawerItem).addClass("sidedrawer-menu-body-item-active");
+}
+
 $(function () {
   scaleToFill();
-
   $(".video-background").on("loadeddata", scaleToFill);
-
   $(window).resize(function () {
     scaleToFill();
   });
